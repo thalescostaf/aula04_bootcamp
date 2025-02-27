b0428e87f40dae3df5187bf1c6654ddae202df9d
Thales Fernandes <contato.thales@gmail.com>
2025-02-27 19:46:37 -0300

    Subindo main com exemplo de tipagem em Python

---
+++ main.py
@@ -0,0 +1,11 @@
+# Variaveis sem tipagem
+idade = 30
+altura = 1.75
+nome = "Thales"
+is_estudante = True
+
+# Variáveis com tipagem
+idade: int = 30
+altura: float = 1.75
+nome:str = "Thales"
+is_estudante:bool = True
