4a01c6cc59e1967a9528851b965ca715195c3804
Thales Fernandes <contato.thales@gmail.com>
2025-02-27 20:08:07 -0300

    Exemplo de lista append e pop

---
+++ exemplo_lista.py
@@ -0,0 +1,20 @@
+produto1: str = "sapato"
+produto2: str = "camiseta"
+produto3: str = "videogame"
+
+produtos: list = []
+
+# Comando para adicionar uma variável na lista (append)
+
+produtos.append(produto1)
+produtos.append(produto2)
+produtos.append(produto3)
+
+print(produtos)
+
+# Comando para retirar o último produto da lista (pop)
+
+produtos.pop()
+
+print(produtos)
+
