0ae402d146ba66879849fa6f91c74b74da813313
Thales Fernandes <contato.thales@gmail.com>
2025-02-27 21:24:20 -0300

    Adicionando exemplo de dicionario

---
+++ exemplo_dic.py
@@ -0,0 +1,17 @@
+# Exemplo de lista
+lista_01 = ['Thales', 'Lais']
+print(lista_01)
+
+# Exemplo de dicionário
+produto_01 = {
+    'nome': 'Thales',
+    'idade': 30
+}
+
+produto_02 = {
+    'nome': 'Lais',
+    'idade': 31
+}
+
+print(produto_01, produto_02)
+
