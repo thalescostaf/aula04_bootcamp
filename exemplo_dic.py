4bab47d196629056bdc8c4e3c25d6bd1ac4c0694
Thales Fernandes <contato.thales@gmail.com>
2025-02-27 21:48:36 -0300

    Atualizando o print da lista

--- exemplo_dic.py
+++ exemplo_dic.py
@@ -18,15 +18,28 @@ produto_02: list = {
 print(produto_01, produto_02)
 
 
-livro: Dict[str, Any] = {
+
+livro_01: Dict[str, Any] = {
     'Título': 'Game of Thrones',
     'Autor': 'Estagiario',
     'Ano': 2005
 }
 
-print(livro)
+livro_02: Dict[str, Any] = {
+    'Título': 'A saga do Estagiario',
+    'Autor': 'Luciano',
+    'Ano': 2004
+}
+
 
 # Como imprimir todos os itens do dicionário
 lista_elementos: list = livro.items()
 for elemento in lista_elementos:
     print(elemento)
+
+
+lista_de_livros = []
+lista_de_livros.append(livro_01)
+lista_de_livros.append(livro_02)
+
+print(lista_de_livros)
