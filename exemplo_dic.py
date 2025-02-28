8efc98c37b787aa8eb838e7080b96e2d9a39f1bb
Thales Fernandes <contato.thales@gmail.com>
2025-02-27 21:36:49 -0300

    Exemplo de dicionários com print

--- exemplo_dic.py
+++ exemplo_dic.py
@@ -1,17 +1,32 @@
+from typing import Dict, Any
+
 # Exemplo de lista
-lista_01 = ['Thales', 'Lais']
+lista_01: list = ['Thales', 'Lais']
 print(lista_01)
 
 # Exemplo de dicionário
-produto_01 = {
+produto_01: list = {
     'nome': 'Thales',
     'idade': 30
 }
 
-produto_02 = {
+produto_02: list = {
     'nome': 'Lais',
     'idade': 31
 }
 
 print(produto_01, produto_02)
 
+
+livro: Dict[str, Any] = {
+    'Título': 'Game of Thrones',
+    'Autor': 'Estagiario',
+    'Ano': 2005
+}
+
+print(livro)
+
+# Como imprimir todos os itens do dicionário
+lista_elementos: list = livro.items()
+for elemento in lista_elementos:
+    print(elemento)
